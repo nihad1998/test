b60bb286b0c309de97e5b3f8fdc0d3c717c1b9d9
Nihad Shukur <nihadshukur1999@gmail.com>
2019-04-08 22:42:12 +0400

    branch test2

---
+++ main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+int x,y;
+x=5;
+y=6;
+printf("%d", x+y);
+return 0;
+}
---
+++ subtract.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+int x=5, y=4;
+printf("%d\n",x-y);
+printf("i changed");
+}
+
+
